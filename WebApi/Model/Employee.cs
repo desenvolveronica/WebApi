f60ca05beb9fe06dfb5baeb34f60e4a7b4d7033e
Verônica Helena <desenvolveronica@gmail.com>
2023-10-31 13:01:53 -0300

    Comentários e explicações

--- WebApi/Model/Employee.cs
+++ WebApi/Model/Employee.cs
@@ -3,15 +3,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApi.Model
 {
-    [Table("employee")]
+    [Table("employee")] //representação da tabela criada no banco
     public class Employee
     {
         [Key]
-        public int id { get; private set; }
+        public int id { get; private set; } 
         public string name { get; private set; }
         public int age { get; private set; }
         public string photo { get; private set; }
+        //props representam as colunas da tabela 
 
+        //como as propriedades são setadas como private é necessário criar o construtor abaixo
         public Employee(string name, int age, string photo)
         {
             this.name = name ?? throw new ArgumentNullException(nameof(name));
